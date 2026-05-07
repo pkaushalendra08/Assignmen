7edc2f7767ae9cc67e545da4343dd9428fd03de3
Kaushalendra Pratap <pkaushalendra08@gmil.com>
2026-05-07 18:10:16 +0530

    implement protected bookmark toggle functionality

--- backend/src/controllers/storyController.js
+++ backend/src/controllers/storyController.js
@@ -1,4 +1,5 @@
 import Story from '../models/Story.js';
+import User from '../models/User.js';
 
 export const getStories = async (req, res) => {
   try {
@@ -32,6 +33,34 @@ export const getStoryById = async (req, res) => {
       return res.status(404).json({ message: 'Story not found' });
     }
     res.status(200).json(story);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const toggleBookmark = async (req, res) => {
+  try {
+    const storyId = req.params.id;
+    const user = await User.findById(req.user._id);
+
+    const story = await Story.findById(storyId);
+    if (!story) {
+      return res.status(404).json({ message: 'Story not found' });
+    }
+
+    const isBookmarked = user.bookmarks.includes(storyId);
+
+    if (isBookmarked) {
+      user.bookmarks = user.bookmarks.filter((id) => id.toString() !== storyId);
+    } else {
+      user.bookmarks.push(storyId);
+    }
+
+    await user.save();
+
+    res.status(200).json({
+      bookmarked: !isBookmarked,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
